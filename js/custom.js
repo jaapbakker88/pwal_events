3550bf75cd1580e08f164c608ed70bf1568de686
Jaap Bakker <hello@bakkerlabs.nl>
2016-12-06 11:16:13 -0500

    further PH customizations

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,11 @@ $(document).ready(function () {
 		$('.producthunt2').show();
 		$('#one .buttons').addClass('kitty');
 		$(".badge").css("background-color", "#da552f");
+		$('.kitty h4 strong').each(function() {
+			var text = $(this).text();
+    		$(this).text(text.replace('people', 'kitties')); 
+    	});
+
 		console.log('xxxxx xxxxxxxxxxxxxxx xxxxx\nxxxx x xxxxxxxxxxxxx x xxxx\nxxx xxxx           xxxx xxx\nxx  xxxxxxxxxxxxxxxxxxx  xx\nxx  xxxx xxxxxxxxx xxxx  xx Happy Hunting!!!!\nxx  xxx x xxxxxxx x xxx  xx\nxx  xxxxxxxxxxxxxxxxxxx  xx Product Hunters!!!\nxxx  xxx  xxxxxxx  xxx  xxx\nxxxx  xxx         xxx  xxxx\nxxxxxx  xxxxxxxxxxx  xxxxxx\nxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
 	}
 });
